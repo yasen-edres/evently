e3430cd71d362fde426c35c74a2b2b73b179fd60
yassin <yase.ehab.230993@gmail.com>
2025-10-10 20:14:59 +0300

    +test

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseFirestore.instance.disableNetwork();
+  await FirebaseFirestore.instance.disableNetwork(); //
   runApp(
     MultiProvider(
       providers: [
